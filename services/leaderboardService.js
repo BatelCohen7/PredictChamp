63e224ae6aa8c796f9c7158f1fa91fe8b306b94e
Batel Cohen <93344134+BatelCohen7@users.noreply.github.com>
2024-07-25 10:58:47 +0300

    Delete services directory

--- services/leaderboardService.js
+++
@@ -1,6 +0,0 @@
-const Leaderboard = require('../models/leaderboardModel');
-
-exports.getLiveLeaderboard = async () => {
-  const leaderboard = await Leaderboard.find().sort('-points').limit(10).populate('user', 'username profileImage');
-  return leaderboard;
-};
