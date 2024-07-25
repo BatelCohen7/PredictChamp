c4de35cd22eb95bf172a611cb55ffdb9e3eb84c9
Batel Cohen <93344134+BatelCohen7@users.noreply.github.com>
2024-07-25 10:58:33 +0300

    Delete routes directory

--- routes/leaderboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const leaderboardController = require('../controllers/leaderboardController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-const router = express.Router();
-
-router.get('/live', authMiddleware, leaderboardController.getLiveLeaderboard);
-
-module.exports = router;
--- routes/notificationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const notificationController = require('../controllers/notificationController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-const router = express.Router();
-
-router.post('/manage', authMiddleware, notificationController.manageNotifications);
-
-module.exports = router;
